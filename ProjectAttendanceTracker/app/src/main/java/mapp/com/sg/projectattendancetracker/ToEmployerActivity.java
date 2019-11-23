ce03ba041ac157ff7205b6fa0d404a1b6f8d5edf
MyridianStar <ameen20.18@ichat.sp.edu.sg>
2019-11-23 19:47:46 +0800

    swapped activity_main buttons id cuz it was TERBALIK

--- ProjectAttendanceTracker/app/src/main/java/mapp/com/sg/projectattendancetracker/ToEmployerActivity.java
+++ ProjectAttendanceTracker/app/src/main/java/mapp/com/sg/projectattendancetracker/ToEmployerActivity.java
@@ -1,7 +1,5 @@
 package mapp.com.sg.projectattendancetracker;
 
-import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ToEmployerActivity extends AppCompatActivity{
