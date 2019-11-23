186fd58f6840c45aca3facbd20064ff58e41f243
MyridianStar <ameen20.18@ichat.sp.edu.sg>
2019-11-23 19:34:34 +0800

    edited manifest

--- ProjectAttendanceTracker/app/src/main/java/mapp/com/sg/projectattendancetracker/MainActivity.java
+++ ProjectAttendanceTracker/app/src/main/java/mapp/com/sg/projectattendancetracker/MainActivity.java
@@ -2,13 +2,35 @@ package mapp.com.sg.projectattendancetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.markAttendance:
+                Intent markAttnIntent = new Intent(this, MarkAttendanceActivity.class);
+                startActivity(markAttnIntent);
+                break;
+
+            case R.id.attendanceSummary:
+                Intent attnSumIntent = new Intent(this, AttendanceSummaryActivity.class);
+                startActivity(attnSumIntent);
+                break;
+
+            case R.id.Employer:
+                Intent toEmployerIntent = new Intent(this, ToEmployerActivity.class);
+                startActivity(toEmployerIntent);
+                break;
+        }
     }
 }
---
+++ ProjectAttendanceTracker/app/src/main/java/mapp/com/sg/projectattendancetracker/ToEmployerActivity.java
@@ -0,0 +1,9 @@
+package mapp.com.sg.projectattendancetracker;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ToEmployerActivity extends AppCompatActivity{
+
+}
