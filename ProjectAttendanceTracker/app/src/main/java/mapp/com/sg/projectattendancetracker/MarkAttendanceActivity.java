e9bda693bff081f5bb01904dde4a679fdb3b3563
MyridianStar <ameen20.18@ichat.sp.edu.sg>
2019-11-23 20:14:40 +0800

    added onCreate for non-homepage activitys

--- ProjectAttendanceTracker/app/src/main/java/mapp/com/sg/projectattendancetracker/MarkAttendanceActivity.java
+++ ProjectAttendanceTracker/app/src/main/java/mapp/com/sg/projectattendancetracker/MarkAttendanceActivity.java
@@ -1,7 +1,13 @@
 package mapp.com.sg.projectattendancetracker;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MarkAttendanceActivity extends AppCompatActivity{
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_markattn);
+    }
 }
