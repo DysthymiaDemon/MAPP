c4b746c8ee00fc953aa2d81b7aae67cd366342d5
Myridian Star <ameenkhan1208@gmail.com>
2020-02-02 01:19:54 +0800

    LoginActivity now identical to wireframe and fully functional
    email passed on in intent to AttnSummActivity

--- ProjectAttendanceTracker/app/src/main/java/mapp/com/sg/projectattendancetracker/AttnSummActivity.java
+++ ProjectAttendanceTracker/app/src/main/java/mapp/com/sg/projectattendancetracker/AttnSummActivity.java
@@ -1,4 +1,23 @@
 package mapp.com.sg.projectattendancetracker;
 
-public class AttnSummActivity {
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AttnSummActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String emailID;
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        if(getIntent().getStringExtra("EMAIL_ID") != null){
+            emailID = getIntent().getStringExtra("EMAIL_ID");
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
--- ProjectAttendanceTracker/app/src/main/java/mapp/com/sg/projectattendancetracker/LoginActivity.java
+++ ProjectAttendanceTracker/app/src/main/java/mapp/com/sg/projectattendancetracker/LoginActivity.java
@@ -1,9 +1,12 @@
 package mapp.com.sg.projectattendancetracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.Menu;
@@ -14,26 +17,27 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //AttnSummActivity will need sharedPreferences, not the login page right now.
 
     private Button buttonLogin;
     private EditText usernameTextbox;
     private EditText passwordTextbox;
     private ProgressDialog progressDialog;
     private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     public void onStart(){
         super.onStart();
-        firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-        if (currentUser != null){
-            Intent i = new Intent(this, AttnSummActivity.class);
-            startActivity(i);
-        }
+        firebaseAuth.addAuthStateListener(authStateListener);
     }
 
     @Override
@@ -45,24 +49,56 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         buttonLogin = (Button) findViewById(R.id.buttonLogin);
         usernameTextbox = (EditText) findViewById(R.id.emailTextbox);
         passwordTextbox = (EditText) findViewById(R.id.passwordTextbox);
-        buttonLogin.setOnClickListener(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        final FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(currentUser != null){
+                    startActivity(
+                            new Intent(LoginActivity.this, AttnSummActivity.class)
+                    );
+                }
+            }
+        };
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser();
+            }
+        });
     }
 
     private void loginUser(){
-        String email = usernameTextbox.getText().toString().trim();
+        final String email = usernameTextbox.getText().toString().trim();
         String password = passwordTextbox.getText().toString().trim();
 
         if (TextUtils.isEmpty(email)){
             Toast.makeText(this,"Please enter your username", Toast.LENGTH_SHORT).show();
             return;
-        }
-        if(TextUtils.isEmpty(password)){
+        } else if (TextUtils.isEmpty(password)){
             Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show();
             return;
+        } else {
+            progressDialog.setMessage("Logging in...");
+            progressDialog.show();
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this,"Sign in failed", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Intent intent = new Intent(LoginActivity.this, AttnSummActivity.class);
+                        intent.putExtra("EMAIL_ID", email);
+                        startActivity(intent);
+                    }
+                }
+            });
         }
-        progressDialog.setMessage("Logging in...");
-        progressDialog.show();
-        //firebaseAuth
+
     }
 
     @Override
